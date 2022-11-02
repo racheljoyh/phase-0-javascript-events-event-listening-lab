161d67a8a40b14eb2df8c9b7f7cef788be2d3751
Rachel <racheljhumes@gmail.com>
2022-11-02 10:17:36 -0600

    commit tree 112b4b3858f47919d8b7666fbb50b8c27bb5ee13

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  let input = document.querySelector('#button')
+  return input.addEventListener('click', alertMe)
+}
+
+function alertMe () {
+  return "I was clicked"
 }
